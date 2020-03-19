2fdfc15f8d1c7dff38b513bf58e97d1ff9a36ebe
MajikoMajiMajiko <Majiko.0718@users.noreply.github.com>
2020-03-19 23:08:40 +0900

    Create LoginDAOTest.java

---
+++ test/LoginDAOTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import dao.LoginDAO;
+import model.Trainer;
+import model.TrainerSearch;
+
+public class LoginDAOTest {
+
+	public static void main(String[] args) {
+		testFindByLogin();
+	}
+
+	public static void testFindByLogin() {
+		TrainerSearch trainerSearch = new TrainerSearch("サトシ", "pikachu");
+		LoginDAO dao = new LoginDAO();
+		Trainer result = dao.findByLogin(trainerSearch);
+
+		//データが存在するかのテスト
+		if(result != null &&
+		result.getId().equals("No.001")  &&
+		result.getTrainername().equals ("サトシ") &&
+		result.getPassword().equals ("pikachu"))
+		{
+			System.out.println("データ見つかりました");
+
+		} else {
+			System.out.println("なんもないよ");
+			}
+		}
+
+}
