76ff257093542f2323e31dac880c9ac40ecbcf11
MajikoMajiMajiko <Majiko.0718@users.noreply.github.com>
2020-03-24 23:18:44 +0900

    Create BackToTopServlet.java

---
+++ servlet/BackToTopServlet.java
@@ -0,0 +1,41 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BackToTopServlet
+ */
+@WebServlet("/BackToTopServlet")
+public class BackToTopServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BackToTopServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//TOPへリダイレクト
+		response.sendRedirect("/Pokemon/jsp/PokemonTop.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+}
