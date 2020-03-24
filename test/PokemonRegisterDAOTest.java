6d0042ed5d9450369562890545d7224da0d42e67
MajikoMajiMajiko <Majiko.0718@users.noreply.github.com>
2020-03-24 23:17:45 +0900

    Create PokemonRegisterDAOTest.java

---
+++ test/PokemonRegisterDAOTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import dao.PokemonRegisterDAO;
+import model.PokeRegister;
+
+public class PokemonRegisterDAOTest {
+
+	public static void main(String[] args) {
+		testInsertData();
+
+	}
+
+	public static void testInsertData() {
+		PokeRegister pokeRegister = new PokeRegister("No.007", "ゼニガメ", "みず", 0.5, 9.0, "げきりゅう", "C:¥Users¥majik¥Pictures¥Pokemon¥zenigame.png");
+		PokemonRegisterDAO dao = new PokemonRegisterDAO();
+		boolean insertResult = dao.insertData(pokeRegister);
+
+		//登録成功したかのテスト
+		if(insertResult == true) {
+			System.out.println("せいこう");
+		} else {
+			System.out.println("しっぱい");
+		}
+	}
+
+}
